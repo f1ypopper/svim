06f030eb713e9b2e35ba433f4e4511fd363d5cac
f1ypopper <patil8atharva@gmail.com>
2023-11-10 16:52:59 +0530

    Update readme

--- README.md
+++ README.md
@@ -10,6 +10,38 @@ svim: A spreadsheet with vim bindings.
 - Formulas
 - Import/Export CSV files
 
+## how to svim
+
+### svimming around
+
+basic up, down, left and right movement can be performed with keys: hjkl.
+- y:  copy a cell value
+- x:  cut a cell value
+- p:  paste a cell value
+- G:  goto first cell of grid
+- g:  goto last cell of grid
+
+### normal mode
+the default mode present, to return to normal from any other mode press Escape
+
+### visual mode
+visual mode can be entered with v and selection can be done using standard motion
+
+### insert mode
+data can be inserted using i and escape to return back to normal mode
+
+### command mode
+command mode starts by colon(:) followed by the command and the arguments, if any
+some commands also work with a selection
+
+#### current commands
+- align - align text inside a cell.
+- bold - makes the text inside a cell bold.
+- fill - fills the n selected cells with numbers from 0 to n.
+- add [row|col] [n] - extends sheet with n cols or rows.
+- export [file_name] - exports the current sheet as a CSV file.
+- [cell_cord] - teleports to the given cell.
+
 ## svim source code
 
 All code currently lies in the src/ directory
--- src/command.js
+++ src/command.js
@@ -11,13 +11,13 @@ function bold(args) {
 
   exitCommandMode();
 }
-function center(args) {
+function align(args) {
   if (isVisual) {
     Selection.map(function (cell) {
-      cell.elem.style.textAlign = "center";
+      cell.elem.style.textAlign = args[0];
     });
   } else {
-    Current.elem.style.textAlign = "center";
+    Current.elem.style.textAlign = args[0];
   }
   exitCommandMode();
 }
@@ -65,48 +65,83 @@ const downloadFile = function (text, fileName) {
 
 const exportCSV = function (args) {
   //Get the min row and col which are not empty
-  let fileName = args[0]+'.csv';
+  let fileName = args[0] + ".csv";
   let data = "";
   for (let row = 0; row < maxRows; row++) {
     let rowText = "";
     for (let col = 0; col < maxCols; col++) {
       rowText += getInputCell(col, row).value;
-      if(col+1 < maxCols){
-        rowText+=',';
+      if (col + 1 < maxCols) {
+        rowText += ",";
       }
     }
     data += rowText;
-    if(row+1 < maxRows){
-      data+='\n';
+    if (row + 1 < maxRows) {
+      data += "\n";
     }
   }
   downloadFile(data, fileName);
 };
 
-function save(args){
+function isDigit(c){
+  return c >= '0' && c <= '9';
+}
+
+function decodeSVF(source) {
+  let flatArray = [];
+  let len = source.length;
+  let i = 0;
+  let value = "";
+  while (i < len) {
+    let multiple = 0;
+    if(isDigit(source[i])){
+      let start = i;
+      while(isDigit(source[i])){
+        i++;
+      }
+      multiple = parseInt(source.substring(start, i));
+    }
+    if (source[i] === '"') {
+      i++;
+      let start = source[i];
+      while (source[i] !== '"') {
+        i++;
+      }
+      value = source.substring(start, i);
+      i++;
+    }
+    while(multiple){
+      flatArray.push(value);
+      multiple++;
+    }
+  }
+  return flatArray;
+}
+
+function save(args) {
   let flatGrid = [];
-  for(let i = 0; i < maxRows; i++){
-    for(let j = 0; j < maxCols; j++){
-      flatGrid.push(getInputCell(j,i).value);
+  for (let i = 0; i < maxRows; i++) {
+    for (let j = 0; j < maxCols; j++) {
+      flatGrid.push(getInputCell(j, i).value);
     }
   }
   let i = 0;
   let len = flatGrid.length;
-  let encoded = '';
-  while(i < len){
+  let encoded = "";
+  while (i < len) {
     let value = flatGrid[i];
     let count = 1;
-    while(value === flatGrid[i+1]){
+    while (value === flatGrid[i + 1]) {
       count++;
       i++;
     }
-    if(count > 1){
-      encoded+=count;
+    if (count > 1) {
+      encoded += count;
     }
-    if(value){
-      encoded+='"'+value+'"';
-    }else{
-      encoded+='E';
+    if (value) {
+      encoded += '"' + value + '"';
+    } else {
+      encoded += "E";
     }
     i++;
   }
@@ -116,7 +151,7 @@ function save(args){
 
 export var commandMap = {
   fill: fill,
-  center: center,
+  align: align,
   bold: bold,
   add: addToGrid,
   export: exportCSV,
